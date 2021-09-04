3dee8dc5eaf3587d9d05dd915f5b6fa8e3efff6e
Jean Park <uc.jinyoungpark@gmail.com>
2021-09-04 20:22:37 +1200

    new change

---
+++ src/js/Counter.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  // state = {
+  //   number: 0
+  // }
+  constructor(props) {
+    super(props);
+    this.handleIncrease = this.handleIncrease.bind(this);
+    this.handleDecrease = this.handleDecrease.bind(this);
+    this.state = {
+      number: 0
+    }
+  }
+
+  handleIncrease() {
+    this.setState({
+      number: this.state.number + 1
+    });
+  }
+
+  handleDecrease() {
+    this.setState({
+      number: this.state.number - 1
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+        <div>Value: {this.state.number}</div>
+        <button onClick={this.handleIncrease}>+</button>
+        <button onClick={this.handleDecrease}>-</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
--- src/myName.js
+++ src/myName.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 
-class MyName extends Component {
-  // static defaultProps = {
-  //   name: 'defaultName'
-  // }
-  render() {
-    return (
-      <div>
-        Hello my name is <b>{this.props.name}</b>
-      </div>
-    );
-  }
-}
+// class MyName extends Component {
+//   // static defaultProps = {
+//   //   name: 'defaultName'
+//   // }
+//   render() {
+//     return (
+//       <div>
+//         Hello my name is <b>{this.props.name}</b>
+//       </div>
+//     );
+//   }
+// }
+//
+// MyName.defaultProps = {
+//   name: 'Default Name'
+// };
 
-MyName.defaultProps = {
-  name: 'Default Name'
+const MyName = ({ name }) => {
+  return (
+    <div>
+      Hello my name is {name}.
+    </div>
+  );
 };
 
 export default MyName;
